e47ae027869b74363c048ad44ea1ae27c1f699f1
samuel soto torres <up210713@alumnos.upa.edu.mx>
2024-02-27 11:45:06 -0600

    JS

--- contador-clicks/app.js
+++ contador-clicks/app.js
@@ -1,8 +1,27 @@
-alert("Hello world!!")
-let a = 1;
-let b = 5;
-let c = a - b;
-alert(c);
+let containerClicks = document.getElementById('containerClicks');
+let btnIncrement = document.querySelector('.btn-primary');
+let btnDecrement = document.querySelector('.btn-secondary');
+let btnReset = document.querySelector('.btn-reset');
+let count = 0;
 
-let btn = document.getElementById("btn");
-console.log("btn");
+document.addEventListener('DOMContentLoaded', () => {
+    containerClicks.innerText = count;
+        btnIncrement.addEventListener("click", function(){
+            count ++;
+            containerClicks.innerText = count;
+        });
+        
+        btnDecrement.onclick = function(){
+            if (count > 0){
+                count --;
+                containerClicks.innerText = count;
+            } else{
+                alert("El contador ya es 0")
+            }
+        };
+        
+        btnReset.addEventListener("click", () =>{
+            count = 0;
+            containerClicks.innerText = count;
+        });
+});
