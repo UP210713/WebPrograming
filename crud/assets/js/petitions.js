05c222b62d6cfb6adf8952edcf6f0a522033c230
samuel soto torres <up210713@alumnos.upa.edu.mx>
2024-04-10 23:26:51 -0600

    CRUD 2.1

--- crud/assets/js/petitions.js
+++ crud/assets/js/petitions.js
@@ -1,9 +1,6 @@
 export async function getAllUsers(){
     const resp = await fetch("api/getUsers.php");
     const json = await resp.json(); 
-
-    console.log(json);
-    
     return json;
 }
 
@@ -11,18 +8,12 @@ export async function getAllUsers(){
 export async function getAllTasks(){
     const resp = await fetch("api/getTasks.php");
     const json = await resp.json(); 
-
-    console.log(json);
-    
     return json;
 }
 
 export async function getAllTask(){
     const resp = await fetch("api/getTask.php");
     const json = await resp.json(); 
-
-    console.log(json);
-    
     return json;
 }
 
